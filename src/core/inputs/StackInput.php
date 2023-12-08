6f9c76ab32eb6295ad2100cb9de9ebb8c383a155
Saaweel <sawelsports@gmail.com>
2023-12-08 12:45:42 +0100

    StackInput type start

--- src/core/inputs/StackInput.php
+++ src/core/inputs/StackInput.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 
 namespace src\core\inputs;
+use src\core\options\StackOptions;
+use src\core\security\StackException;
+
 /**
  * This file is part of the STACK Question plugin for ILIAS, an advanced STEM assessment tool.
  * This plugin is developed and maintained by SURLABS and is a port of STACK Question for Moodle,
@@ -22,5 +25,113 @@ namespace src\core\inputs;
  *********************************************************************/
 class StackInput
 {
+    const STACK_INPUT_BLANK = '';
+    const STACK_INPUT_VALID = 'valid';
+    const STACK_INPUT_INVALID = 'invalid';
+    const STACK_INPUT_SCORE = 'score';
+    const STACK_INPUT_GRAMMAR_FIX_INSERT_STARS = 1;
+    const STACK_INPUT_GRAMMAR_FIX_SPACES = 2;
+    const STACK_INPUT_GRAMMAR_FIX_SINGLE_CHAR = 4;
 
+    private ?string $name;
+
+    private array $context_session = array();
+
+    private ?string $teacher_answer;
+
+    private static array $all_parameter_names = array(
+        'mustVerify',
+        'showValidation',
+        'boxWidth',
+        'boxHeight',
+        'strictSyntax',
+        'insertStars',
+        'syntaxHint',
+        'syntaxAttribute',
+        'forbidWords',
+        'allowWords',
+        'forbidFloats',
+        'lowestTerms',
+        'sameType'
+    );
+
+    private array $extra_options = array(
+        'hideanswer' => false,
+        'allowempty' => false
+    );
+
+    private StackOptions $options;
+
+    private array $parameters = array();
+
+    private array $errors = array();
+
+    private array $raw_contents = array();
+
+    private bool $runtime = true;
+
+    /**
+     * @throws StackException
+     */
+    public function __construct($name, $teacher_answer, $options = null, $parameters = null, $runtime = true) {
+        if (trim($name) === '') {
+            throw new StackException('StackInput: $name must be non-empty.');
+        }
+
+        $this->name = $name;
+        $this->teacher_answer = $teacher_answer;
+        $this->runtime = $runtime;
+
+        if ($options == null || !is_a($options, 'src\core\options\StackOptions')) {
+            throw new StackException('StackInput: $options must be stack_options.');
+        }
+
+        $this->options = $options;
+
+        if ($parameters == null || !is_array($parameters)) {
+            throw new StackException('StackInput: __construct: 3rd argument, $parameters, ' . 'must be null or an array of parameters.');
+        }
+
+        foreach ($parameters as $name => $value) {
+            $this->setParameter($name, $value);
+        }
+
+        $this->internalConstruct();
+    }
+
+    /**
+     * Check if parameter is used
+     */
+    private function isParameterUsed(string $key): bool
+    {
+        return array_key_exists($key, $this->parameters);
+    }
+
+    /**
+     * Set value of parameter
+     * @throws StackException
+     */
+    private function setParameter(string $key, int $value): void
+    {
+        if (!$this->isParameterUsed($key)) {
+            throw new StackException('StackInput: setting parameter ' . $key . ' which does not exist for inputs of type ' . get_class($this));
+        }
+
+        if ($key == 'showValidation' && !$value && $this->isParameterUsed('mustVerify')) {
+            $this->setParameter('mustVerify', 0);
+        }
+
+        $this->parameters[$key] = $value;
+
+        if ($key == 'insertStars') {
+            $this->parameters['grammarAutofixes'] = stack_input_factory::convert_legacy_insert_stars($value);
+        }
+
+        $this->internalConstruct();
+    }
+
+    private function internalConstruct()
+    {
+        // TODO: Implement internalConstruct() method.
+    }
 }
