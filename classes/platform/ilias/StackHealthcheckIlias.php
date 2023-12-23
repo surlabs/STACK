6a231aec9a9ed23d915556ce79659a9e607b93a6
jcopado <jcopado@surlabs.es>
2023-12-23 20:44:47 +0100

    healthcheck structure

--- classes/platform/ilias/StackHealthcheckIlias.php
+++ classes/platform/ilias/StackHealthcheckIlias.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace classes\platform\ilias;
 
+use classes\platform\StackConfig;
+
 /**
  * This file is part of the STACK Question plugin for ILIAS, an advanced STEM assessment tool.
  * This plugin is developed and maintained by SURLABS and is a port of STACK Question for Moodle,
@@ -30,15 +32,63 @@ class StackHealthcheckIlias
      */
     public static function doHealthcheck(): array
     {
-        //TODO: Implement healthcheck
-        /*
-         * En versiones anteriores implementado en:
-         * classes/model/configuration/class.assStackQuestionHealthcheck.php
-         * En ese caso solo hay que devolver los datos a mostrar en la vista
-         * provenientes de maxima o que se hayan obtenido en el healthcheck
-         * la vista se encaragará de mostrarlos
-         */
-        return [];
+        $healthcheck_data = [];
+
+        //Check if the platform config is loaded
+        $healthcheck_data['is_stack_config_loaded'] = self::isStackConfigLoaded();
+
+        //Check if the mbstring extension is loaded
+        $healthcheck_data['is_mbstring_loaded'] = self::isMbstringLoaded();
+
+        //Checks the current supported maxima libraries
+        $healthcheck_data['is_maxima_libraries_supported'] = self::validateMaximaLibraries();
+
+        return $healthcheck_data;
+    }
+
+    /**
+     * Checks if the platform config is loaded
+     * @return array
+     */
+    public static function isStackConfigLoaded(): array
+    {
+        $platform_data = StackConfig::getAll();
+        if (!empty ($platform_data))
+            $data['platform'] = [
+                'type' => 'success',
+                'data' => $platform_data,
+                'message' => 'Platform data retrieved successfully.'
+            ];
+        else {
+            $data['platform'] = [
+                'type' => 'error',
+                'data' => null,
+                'message' => 'Platform data could not be retrieved.'
+            ];
+        }
+        return $data;
+    }
+
+    /**
+     * Checks if the mbstring extension is loaded
+     * @return array
+     */
+    public static function isMbstringLoaded(): array
+    {
+        if (!extension_loaded('mbstring')) {
+            $data = [
+                'type' => 'error',
+                'data' => null,
+                'message' => 'STACK requires the PHP mbstring extension to be used. STACK questions might not work properly until this is installed.',
+            ];
+        } else {
+            $data = [
+                'type' => 'success',
+                'data' => null,
+                'message' => 'The PHP mbstring extension is installed.',
+            ];
+        }
+        return $data;
     }
 
 }
