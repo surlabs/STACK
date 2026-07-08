100244f9d1bae3dab5e30e9183b73695a1b832a0
Saúl Díaz <sauldiazdiaz210@gmail.com>
2026-07-08 15:03:49 +0200

    fix: duplicate default input/PRT pair after ILIAS 9 import/upgrade

--- classes/class.assStackQuestion.php
+++ classes/class.assStackQuestion.php
@@ -360,7 +360,9 @@ class assStackQuestion extends assQuestion implements iQuestionCondition, ilObjQ
         require_once __DIR__ . '/utils/class.assStackQuestionInitialization.php';
         require_once(__DIR__ . '/utils/locallib.php');
 
-        if($owner === -1){
+        $q_id = filter_input(INPUT_GET, 'q_id', FILTER_VALIDATE_INT);
+
+        if($owner === -1 && $q_id <= 0) {
             try{
                 $this->loadStandardQuestion();
             } catch (stack_exception $e){
