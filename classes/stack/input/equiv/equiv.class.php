a944ec5653e7f751c0251437d2fb0a007852e57c
Saúl Díaz <sauldiazdiaz210@gmail.com>
2026-07-27 11:45:10 +0200

    Update equiv.class.php

--- classes/stack/input/equiv/equiv.class.php
+++ classes/stack/input/equiv/equiv.class.php
@@ -618,7 +618,7 @@ class stack_equiv_input extends stack_input {
 
         if (self::INVALID == $state->status) {
             $feedback .= html_writer::tag(
-                'span',
+                'div',
                 stack_string('studentValidation_invalidAnswer'),
                 ['class' => 'alert alert-danger stackinputerror']
             );
